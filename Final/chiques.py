e27d3c591a73cde7e901b08812613e51c846cbc2
AlejandroPerez1992 <69736400+AlejandroPerez1992@users.noreply.github.com>
2021-12-03 21:55:29 -0300

    Update chiques.py

--- Final/chiques.py
+++ Final/chiques.py
@@ -1,6 +1,8 @@
 for i in range (1, 10):
   nombre=input('ingrese su nombre de trolo')
   if nombre = 'ale':
-  print('deja de tocarme el codigo kpo, deja mi inclusion ~~~~')
+    print('deja de tocarme el codigo kpo, deja mi inclusion ~~~~')
+  if nombre = 'lucas':
+    print('la inclusividad es para desincluidos y resentidos sociales ~~~~')
   else
     print('ale es un anti inclusion')
